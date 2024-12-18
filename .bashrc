437bb15c966ca58611924d7222227ca482ac13f9
Cait Barral <cait@caitbarral.com>
2024-12-18 08:28:19 -0600

    a fresh start

---
+++ .bashrc
@@ -0,0 +1,32 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# use cli password prompt for gpg
+GPG_TTY=$(tty)
+export GPG_TTY
+
+export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
+export PATH="$PATH:$GEM_HOME/bin"
+export PATH="$HOME/.rbenv/shims:$PATH"
+. "$HOME/.cargo/env"
+
+set XDG_CURRENT_DESKTOP=sway
+set EDITOR=nvim
+
+alias ls='ls --color=auto'
+alias icat='kitty +kitten icat'
+alias grep='grep --color=auto'
+alias beep="echo $'\a'"
+alias nv='nvim'
+
+function _update_ps1() {
+    PS1=$(powerline-shell $?)
+}
+
+if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+declare -a packages=(
+sway
+wofi
+kitty
+firefox
+neovim
+fzf
+ripgrep
+ttf-cascadia-code-nerd
+ruby
+pavucontrol
+guvcview
+mako
+waybar
+wofi
+wl-clipboard
+btop
+git
+nvtop
+mpv
+swaybg
+swayidle
+swaylock
+thunderbird
+libreoffice
+zola
+pamixer
+kanshi
+python-i3ipc
+newsboat
+w3m
+mpv
+yt-dlp
+slurp
+grim
+noto-fonts-emoji
+)
+sudo pacman -S --noconfirm --needed `echo $(echo "${packages[@]}")`
+
+if ! pacman -Qs yay > /dev/null; then
+  sudo pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+  cd ..
+  rm -rf yay
+fi
+
+yay -S --noconfirm --needed powerline-shell batsignal brightnessctl avizo
+
+sudo cp -rsf ~/dotfiles/config/* ~/.config
+sudo cp -sf ~/dotfiles/.bashrc ~
+sudo cp -sf ~/dotfiles/.gitconfig ~
+mkdir -p ~/dl ~/dev ~/wallpaper ~/docs
